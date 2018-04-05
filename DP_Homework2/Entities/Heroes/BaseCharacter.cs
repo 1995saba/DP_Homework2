4d794165f50576d77be31865d06a5c3ef17bc6b5
Sabina <sabina.orunbaeva@gmail.com>
2018-04-05 13:19:01 +0600

    Add files via upload

---
+++ DP_Homework2/Entities/Heroes/BaseCharacter.cs
@@ -0,0 +1,13 @@
+﻿using DP_Homework2.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP_Homework2.Entities.Heroes
+{
+    public abstract class BaseCharacter:IEntity
+    {
+    }
+}
---
+++ DP_Homework2/Entities/Skills/Skill.cs
@@ -0,0 +1,14 @@
+﻿using DP_Homework2.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP_Homework2.Entities.Skills
+{
+    public abstract class Skill:IEntity
+    {
+        public int Level { get; set; }
+    }
+}
